389f9360d3a17d944d8182799a382dc224f00e75
Neil Horman <nhorman@gmail.com>
2021-01-25 09:33:40 -0500

    Adding addforge command

--- cmds/addforge.go
+++ cmds/addforge.go
@@ -4,28 +4,43 @@ import (
 	"flag"
 	"fmt"
 	"git-forge/config"
+	"os"
 )
 
 func usage() {
 	fmt.Printf("Usage: git forge addforge [options]\n")
 	fmt.Printf("Description: Add a forge type to the global gitconfig\n")
 	fmt.Printf("Options:\n")
-	fmt.Printf("\t -h : This message\n")
+	flag.PrintDefaults()
 }
 
 func AddForgeCmd() error {
 
 	helpopt := flag.Bool("help", false, "display help for addforge command")
+	nameopt := flag.String("name", "", "Name of the forge to add")
+	typeopt := flag.String("type", "", "Type of forge (bitbucket, github, etc)")
+	clone := flag.String("cloneurl", "", "Base url this forge clones from")
+	api := flag.String("apiurl", "", "Base url this forge uses to access REST apis")
+	user := flag.String("user", "", "User name this forge uses for personal access")
+	pass := flag.String("pass", "", "Password used to access this forge")
+
 	flag.Parse()
 
+	gitconfigpath := os.Getenv("HOME") + "/.gitconfig"
 	if *helpopt == true {
 		usage()
 		return nil
 	}
 
-	forgeconfig, err := gitconfig.NewForgeConfig("~/.gitconfig")
+	forgeconfig, err := gitconfig.NewForgeConfig(gitconfigpath)
 	if err != nil {
-		return fmt.Errorf("Failed to open .gitconfig: %s\n", err)
+		return fmt.Errorf("Create forge config failed: %s\n", err)
+	}
+	defer forgeconfig.CommitConfig()
+
+	ferr := forgeconfig.AddForge(*nameopt, *typeopt, *clone, *api, *user, *pass)
+	if ferr != nil {
+		return fmt.Errorf("Failed to add forge: %s\n", ferr)
 	}
 
 	return nil
--- config/gitconfig.go
+++ config/gitconfig.go
@@ -1,33 +1,61 @@
 package gitconfig
 
-import ()
-
-type ForgeType int
-
-const (
-	BITBUCKET ForgeType = iota
-	MAX                 = iota
+import (
+	"fmt"
+	"gopkg.in/ini.v1"
 )
 
 type forgeconfig interface {
-	AddForge(name string, forgetype ForgeType, cloneUrl string, apiUrl string, user string, pass string) error
+	AddForge(name string, forgetype string, cloneUrl string, apiUrl string, user string, pass string) error
 	DelForge(name string) error
+	CommitConfig() error
 }
 
 type ForgeConfig struct {
 	path string
+	cfg  *ini.File
 }
 
 func NewForgeConfig(path string) (*ForgeConfig, error) {
+	cfg, err := ini.Load(path)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to open %s: %s\n", path, err)
+	}
 	return &ForgeConfig{
 		path: path,
+		cfg:  cfg,
 	}, nil
 }
 
-func (*ForgeConfig) AddForge(name string, forgetype ForgeType, cloneUrl string, apiUrl string, user string, pass string) error {
+func (f *ForgeConfig) AddForge(name string, forgetype string, cloneUrl string, apiUrl string, user string, pass string) error {
+	var err error
+	var sec *ini.Section
+
+	sec, err = f.cfg.GetSection("forge \"" + name + "\"")
+	if err == nil {
+		// We found the section, so this is a duplicate
+		return fmt.Errorf("Forge %s can't be created: %s\n", name, err)
+	}
+
+	sec, err = f.cfg.NewSection("forge \"" + name + "\"")
+	if err != nil {
+		return fmt.Errorf("Forge %s can't be created: %s\n", name, err)
+	}
+
+	// Now add our keys
+	sec.NewKey("forgetype", forgetype)
+	sec.NewKey("cloneurl", cloneUrl)
+	sec.NewKey("apiurl", apiUrl)
+	sec.NewKey("user", user)
+	sec.NewKey("pass", pass)
+
 	return nil
 }
 
-func (ForgeConfig) DelForge(name string) error {
+func (f *ForgeConfig) DelForge(name string) error {
 	return nil
+}
+
+func (f *ForgeConfig) CommitConfig() error {
+	return f.cfg.SaveTo(f.path)
 }
