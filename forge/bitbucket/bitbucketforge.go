c4b2956d29585fd3cf5ebc57bcb3879bd0a3c511
Neil Horman <nhorman@gmail.com>
2021-01-27 09:07:15 -0500

    Adding ability to clone repos for bitbucket

--- forge/bitbucket/bitbucketforge.go
+++ forge/bitbucket/bitbucketforge.go
@@ -2,6 +2,11 @@ package bitbucketforge
 
 import (
 	"git-forge/log"
+	"gopkg.in/src-d/go-git.v4"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
 )
 
 type BitBucketForge struct {
@@ -14,6 +19,18 @@ func NewBitBucketForge() *BitBucketForge {
 
 func (f *BitBucketForge) Clone(parentFork bool, url string) error {
 	logging.Forgelog.Printf("%s appears to be a bitbucket forge\n", url)
+
+	dirname := strings.TrimSuffix(path.Base(url), filepath.Ext(path.Base(url)))
+	err := os.Mkdir("./"+dirname, 0755)
+	if err != nil {
+		return err
+	}
+
+	_, clonerr := git.PlainClone("./"+dirname, false, &git.CloneOptions{
+		URL: url})
+	if clonerr != nil {
+		return clonerr
+	}
 
 	return nil
 }
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/bigkevmcd/go-configparser v0.0.0-20210106142102-909504547ead
 	github.com/motemen/go-gitconfig v0.0.0-20160409144229-d53da5028b75
 	gopkg.in/ini.v1 v1.62.0
+	gopkg.in/src-d/go-git.v4 v4.13.1
 )
