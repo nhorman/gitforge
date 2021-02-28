fa55f281249bae63a568c5800a8fb48546db1825
Neil Horman <nhorman@gmail.com>
2021-02-28 16:19:17 -0500

    Implementing createpr command on github

--- forge/github/githubforge.go
+++ forge/github/githubforge.go
@@ -41,6 +41,23 @@ func getRepoSlug(url string) (string, error) {
 	return slug, nil
 }
 
+func getRepoSlugAndOwner(url string) (string, string, string, error) {
+	var owner string
+	base := path.Base(url)
+	slug := strings.TrimSuffix(base, filepath.Ext(base))
+	noslugurl := strings.TrimSuffix(url, base)
+	owner = path.Base(noslugurl)
+
+	// Need to see if we need to trim any git@ crap from the owner string
+	if strings.HasPrefix(owner, "git@") == true {
+		// we have to chop off everthing up to the ':'
+		idx := strings.Index(owner, ":")
+		owner = owner[idx+1:]
+	}
+	finalbaseurl := strings.TrimSuffix(noslugurl, owner)
+	return finalbaseurl, slug, owner, nil
+}
+
 func (f *GitHubForge) cleanup(dirname string) error {
 	return os.RemoveAll(dirname)
 }
@@ -105,17 +122,19 @@ func (f *GitHubForge) Clone(opts forge.CloneOpts) error {
 			return remerr
 		}
 
-		cfg, err := gitconfigset.NewForgeConfigSet()
+		cfg, err := gitconfigset.NewForgeConfigSetInDir(dirname)
 		if err != nil {
 			f.cleanup(dirname)
 			return err
 		}
 		defer cfg.CommitConfig()
+
 		ferr := cfg.AddForgeRemoteSection(f.cfg.Type, "origin", "origin-parent")
 		if ferr != nil {
 			f.cleanup(dirname)
 			return ferr
 		}
+
 	}
 	return nil
 }
@@ -158,6 +177,29 @@ func (f *GitHubForge) CreatePr(opts forge.CreatePrOpts) error {
 	}
 
 	err = f.forge.Push(fconfig.Child.Name, opts.Sbranch, opts.Tbranch)
+	if err != nil {
+		return err
+	}
+
+	transport := &github.BasicAuthTransport{
+		Username: f.cfg.User,
+		Password: f.cfg.Pass,
+	}
+
+	client := github.NewClient(transport.Client())
+	ctx := context.Background()
+	_, pslug, powner, _ := getRepoSlugAndOwner(fconfig.Parent.Url)
+	_, _, cowner, _ := getRepoSlugAndOwner(fconfig.Child.Url)
+
+	newPR := &github.NewPullRequest{
+		Title:               github.String(opts.Title),
+		Head:                github.String(cowner + ":" + opts.Sbranch),
+		Base:                github.String(opts.Tbranch),
+		Body:                github.String(opts.Description),
+		MaintainerCanModify: github.Bool(true),
+	}
+
+	_, _, err = client.PullRequests.Create(ctx, powner, pslug, newPR)
 	if err != nil {
 		return err
 	}
