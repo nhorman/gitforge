58608f0fc8f1bdcc934113ab692c7cfc471806a5
Neil Horman <nhorman@gmail.com>
2021-03-21 11:44:57 -0400

    Catching up github driver to bitbucket

--- forge/github/githubforgeuimodel.go
+++ forge/github/githubforgeuimodel.go
@@ -5,10 +5,10 @@ import (
 	"fmt"
 	"git-forge/configset"
 	"git-forge/forge"
+	"strconv"
+	"time"
 
 	"github.com/google/go-github/v33/github"
-	//"gopkg.in/src-d/go-git.v4"
-	//"gopkg.in/src-d/go-git.v4/config"
 )
 
 func (f *GitHubForge) GetAllPrTitles() ([]forge.PrTitle, error) {
@@ -42,13 +42,60 @@ func (f *GitHubForge) GetAllPrTitles() ([]forge.PrTitle, error) {
 	var titles []forge.PrTitle = make([]forge.PrTitle, 0)
 
 	for _, pr := range prs {
-		titles = append(titles, forge.PrTitle{*pr.Title, *pr.ID})
+		titles = append(titles, forge.PrTitle{*pr.Title, int64(*pr.Number)})
 	}
 
 	return titles, nil
 
 }
 
-func (f *GitHubForge) AddWatchPr(idstring string) error {
-	return fmt.Errorf("Not Implemented Yet")
+func (f *GitHubForge) GetPr(idstring string) (*forge.PR, error) {
+	cfg, err := gitconfigset.NewForgeConfigSet()
+	if err != nil {
+		return nil, err
+	}
+	defer cfg.CommitConfig()
+	fconfig, err := cfg.GetForgeRemoteSection()
+	if err != nil {
+		return nil, err
+	}
+
+	transport := &github.BasicAuthTransport{
+		Username: f.cfg.User,
+		Password: f.cfg.Pass,
+	}
+
+	client := github.NewClient(transport.Client())
+	ctx := context.Background()
+	_, pslug, powner, _ := getRepoSlugAndOwner(fconfig.Parent.Url)
+
+	prnum, _ := strconv.Atoi(idstring)
+	pr, _, perr := client.PullRequests.Get(ctx, powner, pslug, prnum)
+	if perr != nil {
+		return nil, perr
+	}
+
+	retpr := forge.PR{
+		Unread:       true,
+		CurrentToken: pr.UpdatedAt.Format(time.UnixDate),
+		Title:        *pr.Title,
+		PrId:         int64(*pr.Number),
+		PullSpec: forge.PrSpec{
+			Source: forge.PrRemote{
+				URL:        *pr.Head.Repo.GitURL,
+				BranchName: *pr.Head.Ref,
+			},
+			Target: forge.PrRemote{
+				URL:        *pr.Base.Repo.GitURL,
+				BranchName: *pr.Base.Ref,
+			},
+		},
+		Discussions: make([]forge.CommentData, 0),
+	}
+
+	return &retpr, nil
+}
+
+func (f *GitHubForge) RefreshPr(pr *forge.PR) (chan *forge.UpdatedPR, error) {
+	return nil, fmt.Errorf("Not Implemented Yet")
 }
